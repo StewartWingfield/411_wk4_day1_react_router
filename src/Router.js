8ec43420ba88702e71b7bebc1c9327ae6bdcc493
Stewart Wingfield <sww915@gmail.com>
2023-11-09 14:04:21 -0500

    add car details and paths

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
-/* 
-    Write code for React Router here
-Import the Home and About components from 
-the component folder. Assign the default
-path "/" to the Home component and "/about"
-to the About component.
+import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car";
 
-Remember to export the component at the end
-*/
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/car/:id" element={<Car />} />
+    </Routes>
+  );
+}
 
-import React from 'react'
-import { Routes, Route } from 'react-router'
-// Write component imports here //
-
-
-
-// Start Router function here //
+export default Router;
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
-
-// import MUI components here //
-// Container, Paper, Chip //
+import React from "react";
+import cars from "../cars.json";
+import { useParams } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Chip from "@mui/material/Chip";
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+  const { id } = useParams();
+  console.log("id", id);
+  const foundCar = cars.find((car) => car.id == id);
+  console.log("found", foundCar);
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "50px",
+      }}
+    >
+      <Card sx={{ maxWidth: "500px" }}>
+        <CardContent>
+          <h1>{foundCar.Name}</h1>
+          <Chip label={`id: ${foundCar.id}`} />
+          <Chip label={`Name: ${foundCar.Name}`} />
+          <Chip label={`Miles_per_Gallon: ${foundCar.Miles_per_Gallon}`} />
+          <Chip label={`Cylinders: ${foundCar.Cylinders}`} />
+          <Chip label={`Displacement: ${foundCar.Displacement}`} />
+          <Chip label={`Horsepower: ${foundCar.Horsepower}`} />
+          <Chip label={`Weight_in_lbs: ${foundCar.Weight_in_lbs}`} />
+          <Chip label={`Acceleration: ${foundCar.Acceleration}`} />
+          <Chip label={`Year: ${foundCar.Year}`} />
+          <Chip label={`Origin: ${foundCar.Origin}`} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default Car
+export default Car;
