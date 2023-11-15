d1923fbc1213d792a8c70d87e12e2f837d6d803e
Stewart Wingfield <sww915@gmail.com>
2023-11-14 19:19:59 -0500

    Updates

---
+++ precache-manifest.972fbd88ae307100ac011c68c50d55b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c154410f0980ad41055a7c6de0dc1ae",
+    "url": "/411_wk4_day1_react_router/index.html"
+  },
+  {
+    "revision": "6778c01d2de93f159d3a",
+    "url": "/411_wk4_day1_react_router/static/css/main.8e4de9ae.chunk.css"
+  },
+  {
+    "revision": "ed2d99fd8bde8e7731fe",
+    "url": "/411_wk4_day1_react_router/static/js/2.cb4bfd8b.chunk.js"
+  },
+  {
+    "revision": "6778c01d2de93f159d3a",
+    "url": "/411_wk4_day1_react_router/static/js/main.6b49fb08.chunk.js"
+  },
+  {
+    "revision": "0e9dcfd1a95c89889604",
+    "url": "/411_wk4_day1_react_router/static/js/runtime~main.0c971c59.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.a8ae87f13cd5f5d0ddd55c43c0e8deeb.js"
+  "/411_wk4_day1_react_router/precache-manifest.972fbd88ae307100ac011c68c50d55b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/411_wk4_day1_react_router/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
